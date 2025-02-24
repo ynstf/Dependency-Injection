5cdb98268566c583ac10d695a0fff7d326ba33c3
YONES <107154559+ynstf@users.noreply.github.com>
2025-02-24 16:24:09 +0000

    Créer l'interface IDao avec une méthode getData & Créer une implémentation de cette interface

--- Injection/src/main/java/metier/MetierImpl.java
+++ Injection/src/main/java/metier/MetierImpl.java
@@ -1,8 +1,20 @@
 package metier;
 
+import dao.Daoimpl;
+import dao.IDao;
+
 public class MetierImpl implements IMetier {
+    //private IDao dao = new Daoimpl(); //Couplage fort
+
+    private IDao dao; //Couplage faible
     @Override
     public double calcul() {
+        double w_speed = dao.getData();
+        double res = w_speed / 1000;
         return 0;
+    }
+
+    public void setDao(IDao dao){
+        this.dao = dao;
     }
 }
